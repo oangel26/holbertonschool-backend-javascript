46dd9a34b090b28e4791396603fb672bf6f47c62
oangel26 <oa.angel26@gmail.com>
2022-07-28 21:00:17 -0500

    first commit

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,56 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof (name) !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof (length) !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    students.forEach((student) => {
+      if (typeof (student) !== 'string') {
+        throw TypeError('Students must an array of strings');
+      }
+    });
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get getName() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof (name) !== 'string') {
+      throw TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    } else {
+      this._length = length;
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    students.forEach((student) => {
+      if (typeof student !== 'string') {
+        throw TypeError('Students must be an array of strings');
+      } else {
+        this._students = students;
+      }
+    });
+  }
+}
