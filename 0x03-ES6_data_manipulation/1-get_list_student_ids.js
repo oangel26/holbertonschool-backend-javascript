baa8f53da955f98f40904afc6d742f0e737d5322
oangel26 <oa.angel26@gmail.com>
2022-08-02 14:50:53 -0500

    Map() used in array

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,7 @@
 export default function getListStudentIds(array) {
   const lista = [];
   if (Array.isArray(array)) {
-    const myMap = new Map();
-    for (const item of array) {
-      myMap.set(item.id);
-    }
-    for (const key of myMap.keys()) {
-      lista.push(key);
-    }
+      return array.map((item) => item.id);
   }
-  return lista;
+  return [];
 }
