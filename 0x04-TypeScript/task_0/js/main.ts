83a2669dcf1c715eefc22d9d96e6d2b6ac82df3e
oangel26 <oa.angel26@gmail.com>
2022-08-03 18:24:44 -0500

    first commit

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,12 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = new Student('Oscar', 'Angel', 33, 'Cartagena');
+const student2: Student = new Student('Juancho', 'Polo', 30, 'Bogota');
+
+const studentList: any[] = [user1, user2];
+
