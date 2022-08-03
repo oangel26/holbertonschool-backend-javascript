e147f0a6501fed0e09813f77de15744b7943f62e
oangel26 <oa.angel26@gmail.com>
2022-08-02 21:50:11 -0500

    first commit

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,12 @@
 export default function cleanSet(set, startString) {
-  let result = "";
-  if (!startString || !startString.length) return result;
+  let result = '';
+  if (!startString || !startString.length) {
+    return result;
+  }
   set.forEach((i) => {
-    if (i && i.startsWith(startString))
+    if (i && i.startsWith(startString)) {
       result += `${i.slice(startString.length)}-`;
+    }
   });
   return result.slice(0, result.length - 1);
 }
