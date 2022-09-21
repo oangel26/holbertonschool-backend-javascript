962824e129984be72b5d3abe917c714852e509db
oangel26 <oa.angel26@gmail.com>
2022-09-21 17:01:52 -0500

    This is a test is not my code

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,12 @@
+const calculateNumber = require('./0-calcul');
+const assert = require('assert');
+
+describe('calculateNumber', () => {
+  assert.equal(4, calculateNumber(1, 3));
+  assert.equal(5, calculateNumber(1.8, 3));
+  assert.equal(5, calculateNumber(1.8, 3.2));
+  assert.equal(4, calculateNumber(1, 3.2));
+  assert.equal(5, calculateNumber(1, 3.7));
+  assert.equal(5, calculateNumber(1.2, 3.7));
+  assert.equal(6, calculateNumber(1.5, 3.7));
+});
