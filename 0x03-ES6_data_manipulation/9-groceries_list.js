3bb1de2cc202f6b045868de278faa904cc62f4b8
oangel26 <oa.angel26@gmail.com>
2022-10-20 18:46:42 +0100

    Not my code

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default function groceriesList() {
+  const map1 = new Map([
+    ["Apples", 10],
+    ["Tomatoes", 10],
+    ["Pasta", 1],
+    ["Rice", 1],
+    ["Banana", 5]
+  ]);
+  return map1;
+}
