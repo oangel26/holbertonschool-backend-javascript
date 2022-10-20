41ca2711abd654c4b10437cc8ac5ca7578148558
oangel26 <oa.angel26@gmail.com>
2022-10-20 18:44:24 +0100

    Not my code

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default function updateStudentGradeByCity(students, city, newGrades) {
   const studentsByCity = students.filter((student) => student.location === city);
 
